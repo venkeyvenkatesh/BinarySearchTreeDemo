69adf5497462ef0a0aaa4dfcbdc44da910ddd6dc
venkey <venkeyvenkatesh885@gmail.com>
2020-10-22 23:17:54 +0530

    first commit

---
+++ BinarySearchTreeDemo/BinarySearchTree.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTreeDemo
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+
+        public T NodeData { get; set; }
+
+        public BinarySearchTree<T> LeftTree { get; set; }
+
+
+        public BinarySearchTree<T> RightTree { get; set; }
+
+
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.RightTree = null;
+            this.LeftTree = null;
+        }
+
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                this.leftCount++;
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                    //this.leftCount++;
+                }
+                else
+                {
+                    this.LeftTree.Insert(item);
+                   // this.leftCount++;
+
+                }
+            }
+            else
+            {
+                this.rightCount++;
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new BinarySearchTree<T>(item);
+                    
+
+                }
+                else
+                {
+                    this.RightTree.Insert(item);
+                }
+            }
+
+        }
+
+        public void Display()
+        {
+            int left = leftCount;
+            int right = rightCount;
+            if (this.LeftTree != null)
+            {
+                left++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+
+            if (this.RightTree != null)
+            {
+                right++;
+                this.RightTree.Display();
+            }
+
+        }
+
+        public void GetSize()
+        {
+            Console.WriteLine("Size  " + ( this.leftCount + this.rightCount+1));
+
+        }
+
+        public bool Search(T element, BinarySearchTree<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.NodeData.Equals(element))
+            {
+              //  Console.WriteLine("Element Found In BST " + node.NodeData);
+
+                result = true;
+            }
+            else
+            {
+                Console.WriteLine("Current Element is " + node.NodeData);
+
+            }
+
+            if (element.CompareTo(node.NodeData) < 0)
+            {
+                Search(element, node.LeftTree);
+            }
+            if (element.CompareTo(node.NodeData) > 0)
+            {
+                Search(element, node.RightTree);
+            }
+
+            return result;
+
+        }
+
+    }
+
+}
---
+++ BinarySearchTreeDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BinarySearchTreeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Tree Implimentation!");
+
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Insert(22);
+            binarySearchTree.Insert(40);
+            binarySearchTree.Insert(60);
+            binarySearchTree.Insert(95);
+            binarySearchTree.Insert(11);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(16);
+            binarySearchTree.Insert(63);
+            binarySearchTree.Insert(67);
+          //  binarySearchTree.Insert(16);
+
+            binarySearchTree.Display();
+
+            binarySearchTree.GetSize();
+
+            if(binarySearchTree.Search(11, binarySearchTree))
+            {
+                Console.WriteLine("11 is found in BST");
+            }
+            else
+            {
+                Console.WriteLine("Not Found");
+            }
+
+          
+
+        }
+    }
+}
